a7547e245be4e2858be10e9392081187a126c722
unknown <samueltrout42gmail.com>
2025-09-22 11:42:11 -0400

    a

--- player.gd
+++ player.gd
@@ -1,21 +1,31 @@
-#Sam Trout's Love Child
+# Sam Trout's Love Child
 
 extends CharacterBody2D
 
-
-#list of constants that I can change easily for tuning
+# --- Tunable constants ---
 @export var accel = 2400
 @export var jump_velocity = -400
 @export var gravity = 1200
 @export var friction = 1500.0
 @export var max_speed = 200.0
-@export var jump_buffer = 0.1 #seconds
-@export var coyote_time = 0.1 #seconds
+@export var jump_buffer = 0.1 # seconds
+@export var coyote_time = 0.1 # seconds
 
+# --- Timers ---
 var coyote_timer = 0.0
 var jump_buffer_timer = 0.0
 
+# --- Interpolation ---
+var last_position: Vector2
+var current_position: Vector2
+
+func _ready():
+	last_position = global_position
+	current_position = global_position
+
 func _physics_process(delta):
+	last_position = global_position
+
 	var velocity = self.velocity
 
 	# Gravity
@@ -23,46 +33,40 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	else:
 		velocity.y = 0
-		coyote_timer = coyote_time #grounded, so reset coyote timer
+		coyote_timer = coyote_time  # grounded, reset coyote timer
 
-
-	#update timers
+	# Update timers
 	if coyote_timer > 0:
 		coyote_timer -= delta
 	if jump_buffer_timer > 0:
 		jump_buffer_timer -= delta
-	
-	
-	# horizontal movement with acceleration
-	var input_dir = Input.get_axis("ui_left", "ui_right")
 
+	# Horizontal movement with acceleration
+	var input_dir = Input.get_axis("ui_left", "ui_right")
 	if input_dir != 0:
 		velocity.x = move_toward(velocity.x, input_dir * max_speed, accel * delta)
 	else:
-		# friction when no input (slow down precise landings)
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
-
-	#jump queue
+	# Jump queue
 	if Input.is_action_just_pressed("ui_accept"):
-		jump_buffer_timer = jump_buffer #set the timer to land within the buffer
-	
-	# Jump if coyote and buffer
+		jump_buffer_timer = jump_buffer
+
+	# Jump if buffer + coyote overlap
 	if jump_buffer_timer > 0 and coyote_timer > 0:
 		velocity.y = jump_velocity
-		#reset the clocks
-		#if i dont do this, then you could do two jumps instantly and gain huge verticality
-		#i actually remember playing red ball 3, where they did let you do this.
 		jump_buffer_timer = 0
 		coyote_timer = 0
 
-	#jump height
+	# Variable jump height (short hops)
 	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
-		velocity.y *= 0.5   # if jump released, slow down (short hops)
+		velocity.y *= 0.5
 
 	self.velocity = velocity
-	
-	
-func _process(delta):
 	move_and_slide()
-	
+
+	current_position = global_position
+
+func _process(delta):
+	var alpha = Engine.get_physics_interpolation_fraction()
+	global_position = last_position.lerp(current_position, alpha)
