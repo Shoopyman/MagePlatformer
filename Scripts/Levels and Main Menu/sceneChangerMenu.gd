06455d173c98a10732b59cf1e03c3fc190cc37d4
Steven Mejia <boyishtomb519@gmail.com>
2025-10-08 15:35:21 -0400

    fixed main menu buttons

--- Scripts/Levels and Main Menu/sceneChangerMenu.gd
+++ Scripts/Levels and Main Menu/sceneChangerMenu.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 
 func _on_demo_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/demo_stage.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/demo_stage.tscn")
 
 
 func _on_tutorial_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/tutorial.tscn")
 
 
 func _on_delete_later_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level.tscn")
