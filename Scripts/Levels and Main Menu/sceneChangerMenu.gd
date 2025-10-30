86e3787b4a4c8c9714743bb846b6cf7db4673a6f
Steven Mejia <boyishtomb519@gmail.com>
2025-10-29 20:12:39 -0400

    Added button to level one

--- Scripts/Levels and Main Menu/sceneChangerMenu.gd
+++ Scripts/Levels and Main Menu/sceneChangerMenu.gd
@@ -15,3 +15,7 @@ func _on_delete_later_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_level_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/Junkyard.tscn")
