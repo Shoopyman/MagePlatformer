05569b51b0cf5b330e5192cf69ff732dc0d48d97
Steven Mejia <boyishtomb519@gmail.com>
2025-11-22 17:53:34 -0500

    fixed bug of not spawning in 0,0 when finsihing tutorial level then moving to level one

--- Scripts/Levels and Main Menu/sceneChangerMenu.gd
+++ Scripts/Levels and Main Menu/sceneChangerMenu.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _ready() -> void:
+	CheckpointManager.current_scene_progression = Vector2(0,0)
+	CheckpointManager.current_checkpoint_position = Vector2(0,0)
+	pass
+
 
 func _on_demo_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/demo_stage.tscn")
@@ -19,3 +24,4 @@ func _on_button_pressed() -> void:
 
 func _on_level_1_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/Junkyard.tscn")
+	
