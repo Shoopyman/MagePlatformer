3e246db2503cc602a081a7d10f9625a40f77aad3
eurydna <andrew.aknh@gmail.com>
2025-11-27 02:06:53 -0500

    badger badger badger badger badger badger badger badger badger badger badger badger MUSHROOM MUSHROOM

--- Scripts/objects/mushroom.gd
+++ Scripts/objects/mushroom.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 #Constant variables
+@export_enum("red", "brown") var color := "red"
 @export var bounceHeightTuba = 1030
-@export var smallHeight = 150
-@export var walkSpeed = 100
+@export var smallHeight = 300
+@export var walkSpeed = 50
 @export var direction = 1
 @export var accel = 2400
 
@@ -14,61 +15,53 @@ extends CharacterBody2D
 
 # variables
 enum State {WALKING, STUNNED}
-var defualtState: State = State.WALKING
-var current_body: Node = null
-var tubaSlammed = false
-
+var defaultState: State = State.WALKING
 
 func _ready():
-	pass
+	changeState(defaultState)
 	# Setup Timer
-	
-		
+
 func _physics_process(delta): 
 	velocity.y = 0
-	match defualtState:
+	
+	if direction == -1:
+		sprite.flip_h = false
+	else:
+		sprite.flip_h = true
+		
+	match defaultState:
 		State.WALKING:
 			velocity.x = move_toward(velocity.x, direction * walkSpeed, accel * delta)
+		State.STUNNED:
+			velocity.x = 0
 	move_and_slide()
 	if(is_on_wall()):
 		direction *= -1
-	
 
 func changeState(newState: State):
-	defualtState = newState
-	match defualtState:
+	defaultState = newState
+	sprite.stop()
+	sprite.frame = 0
+	match defaultState:
 		State.WALKING:
-			sprite.play("Walking")
+			sprite.play(color + "Walking")
 		State.STUNNED:
-			sprite.play("Bouncing")
-			calcluateBounce(current_body)
+			sprite.play(color + "Bouncing")
 
 #checks if 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass
 	#if(body.is_in_group("player")):
 		#CheckpointManager.load_saved_progression()
-		
-func calcluateBounce(body: Node2D):
-	if(tubaSlammed):
-		current_body.bounce(bounceHeightTuba)
-	else:
-		current_body.bounce(smallHeight)
-	changeState(State.WALKING)
 
 
 func _on_bounce_body_entered(body: Node2D) -> void:
-	if(body.is_in_group("player") && body.is_slamming()):
+	changeState(State.STUNNED)
+	if(body.is_slamming()):
 		print("Player Bounced")
-		current_body = body
-		changeState(State.STUNNED)
-		walkSpeed = 0
-		tubaSlammed = true
-	elif (body.is_in_group("player") && body.is_slamming() == false):
-		current_body = body
-		changeState(State.STUNNED)
-		walkSpeed = 0
+		body.bounce(bounceHeightTuba)
+	else:
+		body.bounce(smallHeight)
 
-
-func _on_bounce_body_exited(body: Node2D) -> void:
-	walkSpeed = 100
+func _on_animated_sprite_2d_animation_finished() -> void:
+	changeState(State.WALKING)
