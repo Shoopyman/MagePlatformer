48a5ac80f987be278a27714d3359cc4b53527662
Twinslayer4 <boyishtomb519@gmail.com>
2025-11-30 21:05:54 -0500

    Made more art for level 2 objects

--- Scripts/Objectsstuff/checkpoint.gd
+++ Scripts/Objectsstuff/checkpoint.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("closed")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		$AnimatedSprite2D.play("activate")
 		CheckpointManager.set_checkpoint(global_position)
